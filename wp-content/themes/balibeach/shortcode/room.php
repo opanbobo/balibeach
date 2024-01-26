a3533664420e57f1c3795fd030cfa6e4c417f1e5
Tofan <tofan.rukmana@bankina.id>
2024-01-27 03:54:59 +0700

    update semua

--- wp-content/themes/balibeach/shortcode/room.php
+++ wp-content/themes/balibeach/shortcode/room.php
@@ -12,33 +12,49 @@ function rk_room( $atts, $content = null ) {
 
     $content = [
         [
-            'title' => 'Deluxe Suite',
-            'content' => 'Indulge in our Deluxe Suite Garden View, a spacious 83-square-meter retreat with a private balcony or terrace. Experience modern elegance seamlessly blending with nature, creating a tranquil escape for your resort stay.',
+            'title' => 'Deluxe ROOM',
+            'content' => 'Discover serenity in our Deluxe Ocean View Room, a spacious 37 sqm haven offering breathtaking sea views. Relax in luxury on a plush king-sized bed, step out onto your private balcony to bask in the beauty of the ocean, and immerse yourself in the perfect coastal escape.',
         ],
         [
-            'title' => 'Corner Suite',
+            'title' => 'Executive Room',
             'content' => 'Unwind in our Corner Suite Garden View at the resort, boasting a spacious 98 square meters, a private balcony or terrace, and a dedicated dining table. This elegantly appointed sanctuary invites you to savor tranquility amid lush surroundings. Immerse yourself in modern comfort and luxury for an extraordinary retreat.',
         ],
         [
-            'title' => 'Premier Suite',
-            'content' => 'Experience lavish comfort in our Premier Suite, a sprawling retreat of 125 square meters at the resort. This grand suite offers an abundance of space, complemented by a private balcony or terrace and the convenience of a dedicated dining table. Immerse yourself in the serene beauty of the resort\'s gardens while enjoying modern amenities. Your stay promises to be an indulgent blend of sophistication and relaxation in this generously appointed suite.',
+            'title' => 'DELUXE SUITE',
+            'content' => 'Elevate your stay in our Deluxe Ocean View Suite, a lavish 81 sqm haven. Enjoy stunning vistas of the ocean from your suite, featuring a king-sized bed, work desk, and a separate sitting area. Immerse yourself in the perfect blend of luxury and breathtaking views for a truly exceptional experience.',
         ],
         [
-            'title' => 'Meru Suite',
-            'content' => 'Elevate your resort experience with the Meru Suite, a luxurious haven spanning an impressive 160 square meters. This spacious retreat boasts a private balcony or terrace, a dedicated dining table, and modern amenities that redefine indulgence. Immerse yourself in the grandeur of this expansive suite, promising an unparalleled blend of comfort and sophistication amid the resort\'s stunning surroundings.',
+            'title' => 'EXECUTIVE SUITES',
+            'content' => 'Experience luxury redefined in our Executive Ocean View Suite, a sprawling 90 sqm retreat. Revel in the breathtaking coastal panoramas from your private haven, complete with a king-sized bed, work desk, and a cozy sitting area. This suite seamlessly combines expansive comfort with captivating ocean views, ensuring a stay that is both indulgent and unforgettable.',
         ],
         [
-            'title' => 'Presidential Suite',
-            'content' => 'Indulge in our Deluxe Suite Garden View, a spacious 83-square-meter retreat with a private balcony or terrace. Experience modern elegance seamlessly blending with nature, creating a tranquil escape for your resort stay.',
+            'title' => 'SANUR SUITE',
+            'content' => 'Indulge in our Sanur Ocean View Suite, a grand 118 sqm escape where coastal charm meets opulence. Unwind in the spacious suite featuring a king-sized bed, work desk, inviting sitting area, and a dedicated dining table. Immerse yourself in the perfect harmony of luxury and panoramic ocean views, creating an experience that transcends ordinary escapes.',
+        ],
+        [
+            'title' => 'ROYAL SUITE',
+            'content' => 'Welcome to our Royal Ocean View Suite, an expansive 135 sqm haven where grandeur meets coastal splendor. Recline in luxury on a king-sized bed, attend to tasks at the work desk, and relax in the inviting sitting area. Unwind further at the dedicated dining table, all while immersing yourself in breathtaking ocean views. Elevate your stay with a perfect blend of space, comfort, and scenic beauty in the Royal Suite.',
+        ],
+        [
+            'title' => 'Bali beach SUITE',
+            'content' => 'Step into unparalleled luxury with our Bali Beach Ocean View Suite, an expansive 173 sqm retreat where sophistication harmonizes with breathtaking coastal panoramas. Indulge in the spacious elegance of a king-sized bed, complemented by a dedicated work desk, inviting sitting area, and a thoughtfully arranged dining table. Immerse yourself in the perfect fusion of opulence and scenic beauty in the Meru Suite, ensuring a stay that transcends ordinary indulgence.',
+        ],
+        [
+            'title' => 'The Presidential Suite',
+            'content' => 'Introducing the epitome of luxury – our Presidential Ocean View Suite, a sprawling 380 sqm haven where opulence meets unrivaled coastal vistas. Experience regal comfort with a king-sized bed, a dedicated work desk, a spacious living room, and an elegant dining area. Revel in the grandeur of panoramic ocean views, creating an extraordinary retreat in our highest category suite.',
         ],
     ];
 
     $images = [
         ['deluxe.jpg', 'corner.jpg', 'premier.jpg', 'meru.jpg'],
-        ['corner.jpg', 'premier.jpg', 'meru.jpg','deluxe.jpg'],
-        ['premier.jpg', 'meru.jpg','deluxe.jpg', 'corner.jpg'],
-        ['meru.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
-        ['presidential.jpg','presidential.jpg','presidential.jpg','presidential.jpg']
+        ['executive.jpg', 'premier.jpg', 'meru.jpg','deluxe.jpg'],
+        ['deluxe-suite.jpg', 'meru.jpg','deluxe.jpg', 'corner.jpg'],
+        ['executive-suite.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
+        ['sanur.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
+        ['royal.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
+        ['bbc-suite.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
+        ['president-suite.jpg','deluxe.jpg', 'corner.jpg', 'premier.jpg'],
+        // ['presidential.jpg','presidential.jpg','presidential.jpg','presidential.jpg']
     ];
 
 
